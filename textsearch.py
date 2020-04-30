ee38d368488586e5812f22403293044ebaf89abb
joecai6 <55220898+joecai6@users.noreply.github.com>
2020-04-30 03:48:06 -0700

    slow text search function

--- textsearch.py
+++ textsearch.py
@@ -1,1 +1,20 @@
 #searches by title in the parsed file
+import csv
+import sys
+
+catalogSize = 20000000
+csv.field_size_limit(catalogSize)
+
+with open("parsed_file.txt",mode='r', encoding="utf8") as libFile:
+    test_input = input("Enter a title : ");
+    f = open("results.txt", mode="w", encoding="utf8")
+    print("searching...")
+    for row in libFile:
+        if test_input in row:
+            f.write(row)
+    f.close
+            
+
+libFile.close()
+
+#to optimize use some sort of data structure to have alphabetical order
